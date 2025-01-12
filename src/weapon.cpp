b80eb40535370bb4af4f64f738b9863c9f6c04c2
Fdai7401 <fdai7401@hs-fulda.de>
2025-01-12 17:12:16 +0100

    Start first work on weapons

---
+++ src/weapon.cpp
@@ -0,0 +1,5 @@
+#include <weapon.hpp>
+
+WeaponTraits largeCaliber (true, false, false, 0, 3);
+
+Weapon machineGun ("Machine Gun", 0, 10, 10, largeCaliber)
---
+++ src/weapon.hpp
@@ -0,0 +1,42 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+#include <string>
+#include <unit.hpp>
+
+enum class WeaponType {
+    SMALL_ARMS = 0,
+    ANTI_TANK,
+    ARTILLERY,
+    ANTI_AIR,
+    MISSILES
+};
+
+struct WeaponTraits {
+    bool can_hit_ground = false;
+    bool can_hit_air = false;
+    bool can_hit_naval = false;
+    int range_min = 1;
+    int range_max = 1;
+
+    WeaponTraits(bool ground, bool air, bool naval, int min_range, int max_range) 
+        : can_hit_ground(ground), can_hit_air(air), can_hit_naval(naval),
+          range_min(min_range), range_max(max_range) {}
+};
+
+
+class Weapon {
+
+    public:
+    Weapon(std::string name, WeaponType type, int damage, int ammo, WeaponTraits caps)
+    : name(name), type(type), base_damage(damage), ammunition(ammo), traits(caps) {}
+
+    private:
+    std::string name;
+    WeaponType type;
+    int base_damage;
+    int ammunition;
+    WeaponTraits traits;
+
+};
+
+#endif
