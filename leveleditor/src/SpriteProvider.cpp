858515f791ed26198ecb3922bc9171299b9433f3
TheUltimateOptimist <digitalConfidence@web.de>
2025-01-28 11:12:18 +0100

    implemented sprite provider

--- leveleditor/src/SpriteProvider.cpp
+++ leveleditor/src/SpriteProvider.cpp
@@ -1,18 +1,52 @@
 #include "SpriteProvider.hpp"
+#include <iostream>
 
 SpriteProvider::SpriteProvider(std::vector<QPixmap> sprites) : sprites(sprites){}
 
 QPixmap SpriteProvider::get_sprite(uint8_t id) {
-    QImage image(16, 16, QImage::Format::Format_ARGB32);
-    for (int y = 0; y < 16; y++) {
-        for (int x = 0; x < 16; x++) {
-            image.setPixel(x, y, 0xFF228B22);
-        }
+    if (id >= 50) {
+        return sprites[id - 20];
     }
-    return QPixmap::fromImage(image);
+    return sprites[id];
 }
 
 SpriteProvider SpriteProvider::from_spritesheet(const std::string path)
 {
-    return SpriteProvider({});
+    HighFive::File file(path, HighFive::File::ReadOnly);
+    std::vector<QPixmap> sprites;
+    sprites.reserve(60);
+
+    //load terrains
+    for (size_t i = 0; i < tile_names.size(); i++) {
+        std::vector<std::vector<std::vector<uint32_t>>> pixels;
+        file.getDataSet("tiles/" + tile_names[i]).read(pixels);
+        sprites.push_back(SpriteProvider::load_pixmap(pixels, 0));
+    }
+
+    // load buildings
+    for (const std::string& faction_name : faction_names) {
+        std::vector<std::vector<std::vector<uint32_t>>> pixels;
+        file.getDataSet("buildings/" + faction_name).read(pixels);
+        for (int i = 0; i < 5; i++) {
+            sprites.push_back(SpriteProvider::load_pixmap(pixels, i));
+        } 
+    }
+    return SpriteProvider(sprites);
+}
+
+QPixmap SpriteProvider::load_pixmap(std::vector<std::vector<std::vector<uint32_t>>> pixels, int index) {
+    int width = pixels[index][0].size();
+    int height = pixels[index].size();
+    QImage image(width, height, QImage::Format::Format_RGBA8888);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            uint32_t color = pixels[index][height - 1 - y][x];
+            uint32_t r = (color >> 24) & 0xFF; // Most significant byte
+            uint32_t g = (color >> 16) & 0xFF; // Second byte
+            uint32_t b = (color >> 8)  & 0xFF; // Third byte
+            uint32_t a = color & 0xFF;         // Least significant byte 
+            image.setPixel(x, y, qRgba(r, g, b, a));
+        }
+    } 
+    return QPixmap::fromImage(image);
 }
--- leveleditor/src/SpriteProvider.hpp
+++ leveleditor/src/SpriteProvider.hpp
@@ -1,5 +1,40 @@
 #include <QPixmap>
 #include <vector>
+#include "highfive/H5File.hpp"
+
+const std::vector<std::string> tile_names({"plain",
+                                  "water",
+                                  "forest",
+                                  "mountain",
+                                  "bridge_horizontal",
+                                  "bridge_vertical",
+                                  "street_horizontal",
+                                  "street_vertical",
+                                  "street_crossing",
+                                  "street_junction_right",
+                                  "street_junction_left",
+                                  "street_junction_down",
+                                  "street_junction_up",
+                                  "street_corner_top_left",
+                                  "street_corner_top_right",
+                                  "street_corner_bottom_left",
+                                  "street_corner_bottom_right",
+                                  "riff",
+                                  "cliff_top",
+                                  "cliff_bottom",
+                                  "cliff_left",
+                                  "cliff_right",
+                                  "cliff_corner_top_left",
+                                  "cliff_corner_top_right",
+                                  "cliff_corner_bottom_left",
+                                  "cliff_corner_bottom_right",
+                                  "cliff_inverse_corner_top_left",
+                                  "cliff_inverse_corner_top_right",
+                                  "cliff_inverse_corner_bottom_left",
+                                  "cliff_inverse_corner_bottom_right"});
+
+const std::vector<std::string> faction_names(
+      {"red", "blue", "yellow", "green", "purple", "neutral"});
 
 class SpriteProvider {
 public: 
@@ -8,5 +43,6 @@ public:
     SpriteProvider static from_spritesheet(const std::string path);
 
 private:
+    static QPixmap load_pixmap(std::vector<std::vector<std::vector<uint32_t>>> pixels, int index);
     std::vector<QPixmap> sprites;
 };
