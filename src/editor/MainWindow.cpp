bd2a4599960a64c46294230409923a20fe59f27c
Nils <nils-jonathan.eckardt@informatik.hs-fulda.de>
2025-01-28 13:54:17 +0100

    Improved Main Window

--- src/editor/MainWindow.cpp
+++ src/editor/MainWindow.cpp
@@ -6,23 +6,28 @@
 #include "TopBar.hpp"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-    // Create the main widget and layout
-    QWidget *centralWidget = new QWidget(this);
-    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+    //CREATE MAIN WINDOW ------------------------------------------
+    QWidget *mainWidget = new QWidget(this);
     setWindowTitle("Level Editor");
     addToolBar(new TopBar("Cooles level", 200, 200, this));
 
-    QGraphicsScene* scene = new QGraphicsScene(this);
+    //CREATE MAP---------------------------------------------------
+    QGraphicsScene *scene = new QGraphicsScene(scene);
     scene->setSceneRect(0, 0, 400, 400); // Set the scene dimensions
     SpriteProvider tileprovider = SpriteProvider::from_spritesheet("../res/spritesheet.h5");
     QPixmap pixmap = tileprovider.get_sprite(0);
     QGraphicsPixmapItem* pixmapitem = scene->addPixmap(pixmap);
-
     // Create a view to display the scene
-    QGraphicsView* view = new QGraphicsView(scene, this);
-    view->setFixedSize(300, 300);
-    view->scale(10, 10);
-    mainLayout->addWidget(view);
+    QGraphicsView* mapWidget = new QGraphicsView(scene, this);
+    mapWidget->scale(10, 10);
 
-    setCentralWidget(centralWidget);
+    //CREATE TOOLBOX-----------------------------------------------
+    QWidget *toolboxWidget = new QWidget(mainWidget);
+
+    //LAYOUT-------------------------------------------------------
+    QHBoxLayout *layout = new QHBoxLayout(mainWidget);
+    layout -> addWidget(mapWidget);
+    layout-> addWidget(toolboxWidget);
+
+    setCentralWidget(mainWidget);
 }
