09efe66606e4d540f239b9ba9d393a0fff476b96
Frederik <frederik@prasch.de>
2025-01-28 03:17:06 +0100

    Let writelevel create a proper test level

--- src/util/writelevel.cpp
+++ src/util/writelevel.cpp
@@ -6,36 +6,27 @@ int main() {
     boost::property_tree::ptree pt;
 
     // Add data to the property tree
-    pt.put("level.width", "20");
-    pt.put("level.height", "20");
-    pt.put("level.name", "Geiles Level");
+    pt.put("level.width", "15");
+    pt.put("level.height", "10");
+    pt.put("level.name", "Alpha");
 
     std::ostringstream xmlStream;
     boost::property_tree::write_xml(xmlStream, pt);
     std::string xml_data = xmlStream.str();
     std::vector<uint8_t> levelmap = {
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            2, 3, 3, 3, 2, 2, 2, 3, 3, 3, 3, 1, 0, 0, 0,
+            0, 2, 3, 0, 0, 0, 3, 3, 3, 3, 0, 4, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 1, 0, 0, 2,
+            0, 0, 0, 0, 76, 76, 51, 3, 0, 0, 0, 1, 0, 3, 3,
+            0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 57, 1, 3, 0, 0,
+            50, 0, 0, 0, 76, 77, 0, 3, 0, 0, 0, 1, 0, 0, 55,
+            0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 1, 3, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 1, 0, 3, 3,
+            2, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 2,
+            3, 3, 3, 3, 0, 3, 2, 2, 3, 2, 3, 1, 3, 2, 2,
         };
-    HighFive::File file("spritesheet.h5", HighFive::File::ReadWrite);
-    file.createDataSet<std::string>("level/metadata", HighFive::DataSpace::From(xmlStream)).write(xml_data);
-    file.createDataSet<uint8_t>("level/tilesarray", HighFive::DataSpace::From(levelmap)).write(levelmap);
+
+    HighFive::File file("level.h5", HighFive::File::Truncate);
+    file.createDataSet<std::string>("metadata", HighFive::DataSpace::From(xmlStream)).write(xml_data);
+    file.createDataSet<uint8_t>("tilesarray", HighFive::DataSpace::From(levelmap)).write(levelmap);
 }
