229ba4b126d25194a538c657f494ab59f82e95d7
TheUltimateOptimist <digitalConfidence@web.de>
2025-01-29 13:28:52 +0100

    removed unused code

--- src/editor/Tile.cpp
+++
@@ -1,8 +0,0 @@
-#include "Tile.hpp"
-#include <iostream>
-
-Tile::Tile(const QPixmap &pixmap, uint8_t id) : QGraphicsPixmapItem(pixmap) {}
-
-void Tile::mousePressEvent(QGraphicsSceneMouseEvent *event) {
-    std::cout << "clicked" << std::endl;
-}
