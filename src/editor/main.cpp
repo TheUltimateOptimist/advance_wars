080b86729fd35e7ee89e579767a0bd4d8dbe069f
Nils <nils-jonathan.eckardt@informatik.hs-fulda.de>
2025-01-30 14:45:02 +0100

    Kommentare in main gesetzt

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -4,21 +4,27 @@
 #include "LevelScene.hpp"
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    SpriteProvider::initialize("../res/spritesheet.h5");
-    if (argc < 2) {
+    //argc 1 ist der Name des Programms
+    if (argc < 2) {         //keine Argumente übergeben
         std::cerr << "Bitte uebergben Sie den Pfad zu dem Level, das sie bearbeiten wollen oder die Breite und Hoehe des Levels, das sie neu erstellen wollen." << std::endl;
         return 1;
     }
-    LevelScene* level;
-    if (argc == 2) {
-        level = LevelScene::fromFile(argv[1]);
-    } else if (argc == 3) {
-        level = LevelScene::empty("Mein Level", std::stoi(argv[1]), std::stoi(argv[2]));
-    } else {
+    if (argc > 3){          //mehr als 2 Argumente übergeben
         std::cerr << "Zuviele Kommandozeilenargumente." << std::endl;
         return 1;
     }
+
+    //Programm beginnt
+    QApplication app(argc, argv);
+    SpriteProvider::initialize("../res/spritesheet.h5");
+    LevelScene* level;
+
+    if (argc == 2) {        //1 Argument übergeben: Datei Pfad
+        level = LevelScene::fromFile(argv[1]);
+    } else {                //2 Argumente übergeben: Breite, Höhe
+        level = LevelScene::empty("Mein Level", std::stoi(argv[1]), std::stoi(argv[2]));
+    }
+    
     MainWindow window(level);
     window.show();
     return app.exec();
