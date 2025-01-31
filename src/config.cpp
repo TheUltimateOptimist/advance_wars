3a777902e48370ef1b637181ebe7c1b4e53e23b6
Frederik <frederik@prasch.de>
2025-01-31 22:01:48 +0100

    Rename members of config to camelCase

--- src/config.cpp
+++ src/config.cpp
@@ -1,223 +1,228 @@
+#include "config.hpp"
+#include "unit.hpp"
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
-#include <unordered_map>
-#include <string>
 #include <iostream>
 #include <stdexcept>
-#include "unit.hpp"
-#include "config.hpp"
+#include <string>
+#include <unordered_map>
 
 namespace advanced_wars
 {
 
-    Config::Config(const std::string &filename)
+Config::Config(const std::string& filename)
+{
+    namespace pt = boost::property_tree;
+    pt::ptree tree;
+    pt::read_xml(filename, tree);
+
+    for (const auto& unit : tree.get_child("Units"))
     {
-        namespace pt = boost::property_tree;
-        pt::ptree tree;
-        pt::read_xml(filename, tree);
+        if (unit.first != "Unit")
+            continue;
+        const auto& unitData = unit.second;
 
-        for (const auto &unit : tree.get_child("Units"))
+        std::string unit_key = unitData.get<std::string>("<xmlattr>.key");
+        try
         {
-            if (unit.first != "Unit")
-                continue;
-            const auto &unitData = unit.second;
+            UnitId unitId = mapUnitKeyToID(unit_key);
 
-            std::string unit_key = unitData.get<std::string>("<xmlattr>.key");
+            m_unitCosts[unitId] = unitData.get<int>("Cost");
+            m_unitMovementPoints[unitId] = unitData.get<int>("MovementPoints");
+            m_unitAmmo[unitId] = unitData.get<int>("Ammo");
+            m_unitMinRange[unitId] = unitData.get<int>("minRange", 0);
+            m_unitMaxRange[unitId] = unitData.get<int>("maxRange", 0);
+
+            std::string movement_type_str = unitData.get<std::string>("MovementType");
             try
             {
-                UnitId unitId = map_unit_key_to_id(unit_key);
-
-                unit_costs[unitId] = unitData.get<int>("Cost");
-                unit_movement_points[unitId] = unitData.get<int>("MovementPoints");
-                unit_ammo[unitId] = unitData.get<int>("Ammo");
-                unit_min_range[unitId] = unitData.get<int>("minRange", 0);
-                unit_max_range[unitId] = unitData.get<int>("maxRange", 0);
-
-                std::string movement_type_str = unitData.get<std::string>("MovementType");
-                try
-                {
-                    unit_movement_type[unitId] = map_movement_type(movement_type_str);
-                }
-                catch (const std::out_of_range &e)
-                {
-                    std::cerr << "Unknown movement type: " << movement_type_str << " for unit key: " << unit_key << std::endl;
-                    continue;
-                }
-
-                for (const auto &weapon : unitData.get_child("Weapons"))
-                {
-                    if (weapon.first == "PrimaryWeapon")
-                    {
-                        unit_primary_weapon[unitId] = weapon.second.get<std::string>("<xmlattr>.name");
-
-                        for (const auto &damage : weapon.second.get_child("DamageTable"))
-                        {
-                            if (damage.first != "Damage")
-                                continue;
-
-                            std::string target_key = damage.second.get<std::string>("<xmlattr>.unitId");
-                            UnitId targetId = map_unit_key_to_id(target_key);
-                            primary_weapon_damage[unitId][targetId] = damage.second.get<int>("<xmlattr>.value");
-                        }
-                    }
-                    else if (weapon.first == "SecondaryWeapon")
-                    {
-                        unit_secondary_weapon[unitId] = weapon.second.get<std::string>("<xmlattr>.name");
-
-                        for (const auto &damage : weapon.second.get_child("DamageTable"))
-                        {
-                            if (damage.first != "Damage")
-                                continue;
-
-                            std::string target_key = damage.second.get<std::string>("<xmlattr>.unitId");
-                            UnitId targetId = map_unit_key_to_id(target_key);
-                            secondary_weapon_damage[unitId][targetId] = damage.second.get<int>("<xmlattr>.value");
-                        }
-                    }
-                }
+                m_unitMovementType[unitId] = mapMovementType(movement_type_str);
             }
-            catch (const std::out_of_range &e)
+            catch (const std::out_of_range& e)
             {
-                //std::cerr << "Unknown unit key: " << unit_key << std::endl;
+                std::cerr << "Unknown movement type: " << movement_type_str
+                          << " for unit key: " << unit_key << std::endl;
                 continue;
             }
+
+            for (const auto& weapon : unitData.get_child("Weapons"))
+            {
+                if (weapon.first == "PrimaryWeapon")
+                {
+                    m_unitPrimaryWeapon[unitId] = weapon.second.get<std::string>("<xmlattr>.name");
+
+                    for (const auto& damage : weapon.second.get_child("DamageTable"))
+                    {
+                        if (damage.first != "Damage")
+                            continue;
+
+                        std::string target_key = damage.second.get<std::string>("<xmlattr>.unitId");
+                        UnitId      targetId = mapUnitKeyToID(target_key);
+                        m_primaryWeaponDamage[unitId][targetId] =
+                            damage.second.get<int>("<xmlattr>.value");
+                    }
+                }
+                else if (weapon.first == "SecondaryWeapon")
+                {
+                    m_unitSecondaryWeapon[unitId] =
+                        weapon.second.get<std::string>("<xmlattr>.name");
+
+                    for (const auto& damage : weapon.second.get_child("DamageTable"))
+                    {
+                        if (damage.first != "Damage")
+                            continue;
+
+                        std::string target_key = damage.second.get<std::string>("<xmlattr>.unitId");
+                        UnitId      targetId = mapUnitKeyToID(target_key);
+                        m_secondaryWeaponDamage[unitId][targetId] =
+                            damage.second.get<int>("<xmlattr>.value");
+                    }
+                }
+            }
         }
-    }
-
-    UnitId Config::map_unit_key_to_id(const std::string &unit_key) const
-    {
-        static const std::unordered_map<std::string, UnitId> unit_map = {
-            {"infantry", UnitId::INFANTERY},
-            {"mechanized_infantry", UnitId::MECHANIZED_INFANTERY},
-            {"recon", UnitId::RECON},
-            {"apc", UnitId::APC},
-            {"anti_air_tank", UnitId::ANTI_AIR_TANK},
-            {"medium_tank", UnitId::MEDIUM_TANK},
-            {"heavy_tank", UnitId::HEAVY_TANK},
-            {"neotank", UnitId::NEO_TANK},
-            {"artillery", UnitId::ARTILLERY},
-            {"rocket_artillery", UnitId::ROCKET_ARTILLERY},
-            {"anti_air_missile_launcher", UnitId::ANTI_AIR_MISSILE_LAUNCHER},
-            {"lander", UnitId::LANDER},
-            {"cruiser", UnitId::CRUISER},
-            {"submarine", UnitId::SUBMARINE},
-            {"battleship", UnitId::BATTLESHIP},
-            {"transport_helicopter", UnitId::TRANSPORT_HELICOPTER},
-            {"battle_helicopter", UnitId::BATTLE_HELICOPTER},
-            {"fighter", UnitId::FIGHTER},
-            {"bomber", UnitId::BOMBER}};
-
-        auto it = unit_map.find(unit_key);
-        if (it != unit_map.end())
+        catch (const std::out_of_range& e)
         {
-            return it->second;
+            // std::cerr << "Unknown unit key: " << unit_key << std::endl;
+            continue;
         }
-        throw std::out_of_range("Unknown unit key: " + unit_key);
     }
+}
 
-    MovementType Config::map_movement_type(const std::string &movementTypeStr) const
-    {
-        static const std::unordered_map<std::string, MovementType> movement_map = {
-            {"Foot", MovementType::FOOT},
-            {"Wheeled", MovementType::WHEELED},
-            {"Tread", MovementType::TREAD},
-            {"Air", MovementType::AIR},
-            {"Sea", MovementType::SEA},
-            {"Lander", MovementType::LANDER}};
-
-        auto it = movement_map.find(movementTypeStr);
-        if (it != movement_map.end())
-        {
-            return it->second;
-        }
-        throw std::out_of_range("Unknown movement type: " + movementTypeStr);
-    }
-
-
-int Config::get_unit_cost(UnitId id) const
+UnitId Config::mapUnitKeyToID(const std::string& unit_key) const
 {
-    auto it = unit_costs.find(id);
-    if (it != unit_costs.end())
+    static const std::unordered_map<std::string, UnitId> unit_map = {
+        {                 "infantry",                 UnitId::INFANTERY},
+        {      "mechanized_infantry",      UnitId::MECHANIZED_INFANTERY},
+        {                    "recon",                     UnitId::RECON},
+        {                      "apc",                       UnitId::APC},
+        {            "anti_air_tank",             UnitId::ANTI_AIR_TANK},
+        {              "medium_tank",               UnitId::MEDIUM_TANK},
+        {               "heavy_tank",                UnitId::HEAVY_TANK},
+        {                  "neotank",                  UnitId::NEO_TANK},
+        {                "artillery",                 UnitId::ARTILLERY},
+        {         "rocket_artillery",          UnitId::ROCKET_ARTILLERY},
+        {"anti_air_missile_launcher", UnitId::ANTI_AIR_MISSILE_LAUNCHER},
+        {                   "lander",                    UnitId::LANDER},
+        {                  "cruiser",                   UnitId::CRUISER},
+        {                "submarine",                 UnitId::SUBMARINE},
+        {               "battleship",                UnitId::BATTLESHIP},
+        {     "transport_helicopter",      UnitId::TRANSPORT_HELICOPTER},
+        {        "battle_helicopter",         UnitId::BATTLE_HELICOPTER},
+        {                  "fighter",                   UnitId::FIGHTER},
+        {                   "bomber",                    UnitId::BOMBER}
+    };
+
+    auto it = unit_map.find(unit_key);
+    if (it != unit_map.end())
+    {
+        return it->second;
+    }
+    throw std::out_of_range("Unknown unit key: " + unit_key);
+}
+
+MovementType Config::mapMovementType(const std::string& movementTypeStr) const
+{
+    static const std::unordered_map<std::string, MovementType> movement_map = {
+        {   "Foot",    MovementType::FOOT},
+        {"Wheeled", MovementType::WHEELED},
+        {  "Tread",   MovementType::TREAD},
+        {    "Air",     MovementType::AIR},
+        {    "Sea",     MovementType::SEA},
+        { "Lander",  MovementType::LANDER}
+    };
+
+    auto it = movement_map.find(movementTypeStr);
+    if (it != movement_map.end())
+    {
+        return it->second;
+    }
+    throw std::out_of_range("Unknown movement type: " + movementTypeStr);
+}
+
+int Config::getUnitCost(UnitId id) const
+{
+    auto it = m_unitCosts.find(id);
+    if (it != m_unitCosts.end())
     {
         return it->second;
     }
     throw std::runtime_error("Cost for unit ID not found");
 }
 
-int Config::get_unit_movement_points(UnitId id) const
+int Config::getUnitMovementPoints(UnitId id) const
 {
-    auto it = unit_movement_points.find(id);
-    if (it != unit_movement_points.end())
+    auto it = m_unitMovementPoints.find(id);
+    if (it != m_unitMovementPoints.end())
     {
         return it->second;
     }
     throw std::runtime_error("Movement points for unit ID not found");
 }
 
-MovementType Config::get_unit_movement_type(UnitId id) const
+MovementType Config::getUnitMovementType(UnitId id) const
 {
-    auto it = unit_movement_type.find(id);
-    if (it != unit_movement_type.end())
+    auto it = m_unitMovementType.find(id);
+    if (it != m_unitMovementType.end())
     {
         return it->second;
     }
     throw std::runtime_error("Movement type for unit ID not found");
 }
 
-int Config::get_unit_ammo(UnitId id) const
+int Config::getUnitAmmo(UnitId id) const
 {
-    auto it = unit_ammo.find(id);
-    if (it != unit_ammo.end())
+    auto it = m_unitAmmo.find(id);
+    if (it != m_unitAmmo.end())
     {
         return it->second;
     }
     throw std::runtime_error("Ammo for unit ID not found");
 }
 
-int Config::get_unit_min_range(UnitId id) const
+int Config::getUnitMinRange(UnitId id) const
 {
-    auto it = unit_min_range.find(id);
-    if (it != unit_min_range.end())
+    auto it = m_unitMinRange.find(id);
+    if (it != m_unitMinRange.end())
     {
         return it->second;
     }
     throw std::runtime_error("Min range for unit ID not found");
 }
 
-int Config::get_unit_max_range(UnitId id) const
+int Config::getUnitMaxRange(UnitId id) const
 {
-    auto it = unit_max_range.find(id);
-    if (it != unit_max_range.end())
+    auto it = m_unitMaxRange.find(id);
+    if (it != m_unitMaxRange.end())
     {
         return it->second;
     }
     throw std::runtime_error("Max range for unit ID not found");
 }
 
-std::string Config::get_unit_primary_weapon(UnitId id) const
+std::string Config::getUnitPrimaryWeapon(UnitId id) const
 {
-    auto it = unit_primary_weapon.find(id);
-    if (it != unit_primary_weapon.end())
+    auto it = m_unitPrimaryWeapon.find(id);
+    if (it != m_unitPrimaryWeapon.end())
     {
         return it->second;
     }
     throw std::runtime_error("Primary weapon for unit ID not found");
 }
 
-std::string Config::get_unit_secondary_weapon(UnitId id) const
+std::string Config::getUnitSecondaryWeapon(UnitId id) const
 {
-    auto it = unit_secondary_weapon.find(id);
-    if (it != unit_secondary_weapon.end())
+    auto it = m_unitSecondaryWeapon.find(id);
+    if (it != m_unitSecondaryWeapon.end())
     {
         return it->second;
     }
     throw std::runtime_error("Secondary weapon for unit ID not found");
 }
 
-int Config::get_unit_primary_weapon_damage(UnitId attackerid, UnitId defenderid) const
+int Config::getUnitPrimaryWeaponDamage(UnitId attackerid, UnitId defenderid) const
 {
-    auto it = primary_weapon_damage.find(attackerid);
-    if (it != primary_weapon_damage.end())
+    auto it = m_primaryWeaponDamage.find(attackerid);
+    if (it != m_primaryWeaponDamage.end())
     {
         auto damageIt = it->second.find(defenderid);
         if (damageIt != it->second.end())
@@ -225,13 +230,14 @@ int Config::get_unit_primary_weapon_damage(UnitId attackerid, UnitId defenderid)
             return damageIt->second;
         }
     }
-    throw std::runtime_error("Primary weapon damage not found for given attacker/defender combination");
+    throw std::runtime_error(
+        "Primary weapon damage not found for given attacker/defender combination");
 }
 
-int Config::get_unit_secondary_weapon_damage(UnitId attackerid, UnitId defenderid) const
+int Config::getUnitSecondaryWeaponDamage(UnitId attackerid, UnitId defenderid) const
 {
-    auto it = secondary_weapon_damage.find(attackerid);
-    if (it != secondary_weapon_damage.end())
+    auto it = m_secondaryWeaponDamage.find(attackerid);
+    if (it != m_secondaryWeaponDamage.end())
     {
         auto damageIt = it->second.find(defenderid);
         if (damageIt != it->second.end())
@@ -239,6 +245,7 @@ int Config::get_unit_secondary_weapon_damage(UnitId attackerid, UnitId defenderi
             return damageIt->second;
         }
     }
-    throw std::runtime_error("Secondary weapon damage not found for given attacker/defender combination");
+    throw std::runtime_error(
+        "Secondary weapon damage not found for given attacker/defender combination");
 }
-}
+} // namespace advanced_wars
--- src/config.hpp
+++ src/config.hpp
@@ -1,114 +1,117 @@
 #pragma once
 
-#include <unordered_map>
-#include <string>
-#include <stdexcept>
+#include "unit.hpp" // Include for UnitId and MovementType
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
-#include "unit.hpp" // Include for UnitId and MovementType
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
 
 namespace advanced_wars
 {
-    /**
-     * @class Config
-     * @brief Parses and stores unit configuration data from an XML file.
-     * 
-     * This class reads unit attributes, movement types, weapon data, and damage tables
-     * from an XML configuration file and provides access to this data through getter methods.
-     */
-    class Config
-    {
+/**
+ * @class Config
+ * @brief Parses and stores unit configuration data from an XML file.
+ *
+ * This class reads unit attributes, movement types, weapon data, and damage tables
+ * from an XML configuration file and provides access to this data through getter methods.
+ */
+class Config
+{
     public:
         /**
          * @brief Constructs a Config object and loads data from an XML file.
-         * 
+         *
          * The constructor reads the XML file, extracts unit properties, movement types,
          * and weapon damage tables, and stores them in internal data structures.
-         * 
+         *
          * @param filename Path to the XML configuration file.
          * @throws std::runtime_error if the file cannot be read or parsed.
          */
-        explicit Config(const std::string &filename);
+        explicit Config(const std::string& filename);
 
         /** @brief Retrieves the cost of a given unit type. */
-        int get_unit_cost(UnitId id) const;
-        
+        int getUnitCost(UnitId id) const;
+
         /** @brief Retrieves the movement points of a given unit type. */
-        int get_unit_movement_points(UnitId id) const;
-        
+        int getUnitMovementPoints(UnitId id) const;
+
         /** @brief Retrieves the maximum ammunition capacity of a given unit type. */
-        int get_unit_ammo(UnitId id) const;
-        
+        int getUnitAmmo(UnitId id) const;
+
         /** @brief Retrieves the minimum attack range of a given unit type. */
-        int get_unit_min_range(UnitId id) const;
-        
+        int getUnitMinRange(UnitId id) const;
+
         /** @brief Retrieves the maximum attack range of a given unit type. */
-        int get_unit_max_range(UnitId id) const;
-        
+        int getUnitMaxRange(UnitId id) const;
+
         /** @brief Retrieves the name of the primary weapon of a given unit type. */
-        std::string get_unit_primary_weapon(UnitId id) const;
-        
+        std::string getUnitPrimaryWeapon(UnitId id) const;
+
         /** @brief Retrieves the name of the secondary weapon of a given unit type. */
-        std::string get_unit_secondary_weapon(UnitId id) const;
-        
-        /** @brief Retrieves the damage value of a unit's primary weapon against a target unit type. */
-        int get_unit_primary_weapon_damage(UnitId attackerid, UnitId defenderid) const;
-        
-        /** @brief Retrieves the damage value of a unit's secondary weapon against a target unit type. */
-        int get_unit_secondary_weapon_damage(UnitId attackerid, UnitId defenderid) const;
-        
+        std::string getUnitSecondaryWeapon(UnitId id) const;
+
+        /** @brief Retrieves the damage value of a unit's primary weapon against a target unit type.
+         */
+        int getUnitPrimaryWeaponDamage(UnitId attackerid, UnitId defenderid) const;
+
+        /** @brief Retrieves the damage value of a unit's secondary weapon against a target unit
+         * type. */
+        int getUnitSecondaryWeaponDamage(UnitId attackerid, UnitId defenderid) const;
+
         /** @brief Retrieves the movement type of a given unit type. */
-        MovementType get_unit_movement_type(UnitId id) const;
-    
+        MovementType getUnitMovementType(UnitId id) const;
+
     private:
         /** @brief Maps unit IDs to their cost values. */
-        std::unordered_map<UnitId, int> unit_costs;
-        
+        std::unordered_map<UnitId, int> m_unitCosts;
+
         /** @brief Maps unit IDs to their movement points. */
-        std::unordered_map<UnitId, int> unit_movement_points;
-        
+        std::unordered_map<UnitId, int> m_unitMovementPoints;
+
         /** @brief Maps unit IDs to their maximum ammunition capacity. */
-        std::unordered_map<UnitId, int> unit_ammo;
-        
+        std::unordered_map<UnitId, int> m_unitAmmo;
+
         /** @brief Maps unit IDs to their minimum attack range. */
-        std::unordered_map<UnitId, int> unit_min_range;
-        
+        std::unordered_map<UnitId, int> m_unitMinRange;
+
         /** @brief Maps unit IDs to their maximum attack range. */
-        std::unordered_map<UnitId, int> unit_max_range;
-        
+        std::unordered_map<UnitId, int> m_unitMaxRange;
+
         /** @brief Maps unit IDs to their primary weapon names. */
-        std::unordered_map<UnitId, std::string> unit_primary_weapon;
-        
+        std::unordered_map<UnitId, std::string> m_unitPrimaryWeapon;
+
         /** @brief Maps unit IDs to their secondary weapon names. */
-        std::unordered_map<UnitId, std::string> unit_secondary_weapon;
-        
+        std::unordered_map<UnitId, std::string> m_unitSecondaryWeapon;
+
         /** @brief Stores primary weapon damage values for attacker-defender unit combinations. */
-        std::unordered_map<UnitId, std::unordered_map<UnitId, int>> primary_weapon_damage;
-        
+        std::unordered_map<UnitId, std::unordered_map<UnitId, int>> m_primaryWeaponDamage;
+
         /** @brief Stores secondary weapon damage values for attacker-defender unit combinations. */
-        std::unordered_map<UnitId, std::unordered_map<UnitId, int>> secondary_weapon_damage;
-        
+        std::unordered_map<UnitId, std::unordered_map<UnitId, int>> m_secondaryWeaponDamage;
+
         /** @brief Maps unit IDs to their movement types. */
-        std::unordered_map<UnitId, MovementType> unit_movement_type;
-    
+        std::unordered_map<UnitId, MovementType> m_unitMovementType;
+
         /**
          * @brief Converts a unit key string from the XML file to its corresponding UnitId.
-         * 
+         *
          * If the key is unknown, it returns UnitId::UNKNOWN.
-         * 
+         *
          * @param unit_key The string key representing a unit type.
          * @return The corresponding UnitId.
          */
-        UnitId map_unit_key_to_id(const std::string &unit_key) const;
-    
+        UnitId mapUnitKeyToID(const std::string& unit_key) const;
+
         /**
-         * @brief Converts a movement type string from the XML file to its corresponding MovementType.
-         * 
+         * @brief Converts a movement type string from the XML file to its corresponding
+         * MovementType.
+         *
          * If the movement type is unknown, it returns MovementType::UNKNOWN.
-         * 
+         *
          * @param movementTypeStr The string representation of the movement type.
          * @return The corresponding MovementType.
          */
-        MovementType map_movement_type(const std::string &movementTypeStr) const;
-    };
-}
+        MovementType mapMovementType(const std::string& movementTypeStr) const;
+};
+} // namespace advanced_wars
