6b7c42f908bc062c702ec6ed72915a4215077dcd
Nicolas Will <nicolas.will01@gmail.com>
2025-01-31 15:00:11 +0100

    fix naming of members in units

--- src/unit.cpp
+++ src/unit.cpp
@@ -83,18 +83,18 @@ void Unit::attack(Unit* enemy)
 {
     // Angenommen, primary_weapon und secondary_weapon wurden bereits korrekt
     // initialisiert
-    auto primary_weapon_damage_it = primary_weapon.damage.find(enemy->m_id);
-    auto secondary_weapon_damage_it = secondary_weapon.damage.find(enemy->m_id);
+    auto primary_weapon_damage_it = m_primary_weapon.m_damage.find(enemy->m_id);
+    auto secondary_weapon_damage_it = m_secondary_weapon.m_damage.find(enemy->m_id);
 
     int attacker_damage_value = 0;
 
     // Die Waffe mit dem höchsten Schaden wählen
-    if (secondary_weapon_damage_it != secondary_weapon.damage.end())
+    if (secondary_weapon_damage_it != m_secondary_weapon.m_damage.end())
     {
         attacker_damage_value = secondary_weapon_damage_it->second;
     }
 
-    if (primary_weapon_damage_it != primary_weapon.damage.end())
+    if (primary_weapon_damage_it != m_primary_weapon.m_damage.end())
     {
         if (primary_weapon_damage_it->second > attacker_damage_value)
         {
@@ -121,18 +121,19 @@ void Unit::attack(Unit* enemy)
         if (enemy->m_health > 0)
         {
             // Weapon tables for the defender
-            auto defender_primary_weapon_damage_it = enemy->primary_weapon.damage.find(m_id);
-            auto defender_secondary_weapon_damage_it = enemy->secondary_weapon.damage.find(m_id);
+            auto defender_primary_weapon_damage_it = enemy->m_primary_weapon.m_damage.find(m_id);
+            auto defender_secondary_weapon_damage_it =
+                enemy->m_secondary_weapon.m_damage.find(m_id);
 
             int defender_damage_value = 0; // Declare outside for later use
 
             // Determine the damage value for the defender
-            if (defender_secondary_weapon_damage_it != enemy->secondary_weapon.damage.end())
+            if (defender_secondary_weapon_damage_it != enemy->m_secondary_weapon.m_damage.end())
             {
                 defender_damage_value = defender_secondary_weapon_damage_it->second;
             }
 
-            if (defender_primary_weapon_damage_it != enemy->primary_weapon.damage.end())
+            if (defender_primary_weapon_damage_it != enemy->m_primary_weapon.m_damage.end())
             {
                 if (defender_primary_weapon_damage_it->second > defender_damage_value)
                 {
