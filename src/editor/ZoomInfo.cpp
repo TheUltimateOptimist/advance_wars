f2610d530b859d4a417af69120a2d54cde235395
TheUltimateOptimist <digitalConfidence@web.de>
2025-02-03 00:21:39 +0100

    formatted

--- src/editor/ZoomInfo.cpp
+++ src/editor/ZoomInfo.cpp
@@ -3,18 +3,19 @@
 namespace editor
 {
 
-ZoomInfo::ZoomInfo(QWidget *parent) : QPushButton(parent), current_scale(2)
+ZoomInfo::ZoomInfo(QWidget* parent) : QPushButton(parent), current_scale(2)
 {
     setDisabled(true);
-    setText((std::to_string((int) std::round(current_scale*100)) + "%").c_str());
+    setText((std::to_string((int)std::round(current_scale * 100)) + "%").c_str());
     setFixedWidth(50);
 }
 
 void ZoomInfo::onZoomed(double delta)
 {
-    if (current_scale + delta <= 0) return;
+    if (current_scale + delta <= 0)
+        return;
     current_scale += delta;
-    setText((std::to_string((int) std::round(current_scale*100)) + " %").c_str());
+    setText((std::to_string((int)std::round(current_scale * 100)) + " %").c_str());
 }
 
 } // namespace editor
