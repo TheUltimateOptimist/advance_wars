be757576a3f89495cf76ec5c3baf19d0fe10b919
Nicolas Will <nicolas.will01@gmail.com>
2025-02-04 10:44:20 +0100

    remove unused member an funktionts in level.hpp

--- src/game/Level.hpp
+++ src/game/Level.hpp
@@ -68,38 +68,28 @@ class Level : public Scene
         Effect removeEffect(int id);
 
     private:
-        std::string m_name;
-        int         m_width;
-        int         m_height;
-
+        std::string                       m_name;
+        int                               m_width;
+        int                               m_height;
         std::vector<Tile>                 m_tiles;
         std::unordered_map<int, Building> m_buildings;
         std::unordered_map<int, Unit>     m_units;
         std::unordered_map<int, Effect>   m_effects;
-
-        int m_selectedUnit;
-        int m_targetedUnit;
-        int m_selectedBuilding;
-
-        ContextMenu m_contextMenu;
-        bool        m_contextMenuActive;
-
-        int        m_id;
-        LevelState m_state;
+        int                               m_selectedUnit;
+        int                               m_selectedBuilding;
+        ContextMenu                       m_contextMenu;
+        int                               m_id;
+        LevelState                        m_state;
 
         std::pair<int, int> calcTilePos(int mouseX, int mouseY);
         void                selectEntity(int x, int y);
         int                 selectUnit(int tileX, int tileY);
-        bool                targetUnit(int tileX, int tileY);
         int                 selectBuilding(int tileX, int tileY);
 
         void handleSelectingEvents(Engine& engine, SDL_Event& event);
         void handleMenuActiveEvents(Engine& engine, SDL_Event& event);
         void handleMovementEvents(Engine& engine, SDL_Event& event);
         void handleAttackingEvents(Engine& engine, SDL_Event& event);
-
-        bool clickCheckLeft(int mouseX, int mouseY);
-        bool clickCheckRight(int mouseX, int mouseY);
 };
 
 } // namespace advanced_wars
