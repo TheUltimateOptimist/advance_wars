857e699a81a34be62b0efdb0bf3fc54d5aeec40a
Max Cherris <MCherris@protonmail.com>
2025-02-05 17:07:42 +0100

    Add smth

--- src/game/Level.cpp
+++ src/game/Level.cpp
@@ -217,10 +217,6 @@ void Level::render(Engine& engine)
 
     if (m_state == LevelState::RECRUITING_STATE) 
     {   
-        std::pair<int, int> tilePos = m_currentPos.getPosition();
-        m_recruitingMenu.update(
-                    (tilePos.first * 16 + 15) * RENDERING_SCALE,
-                    (tilePos.second * 16 + 15) * RENDERING_SCALE);
         m_recruitingMenu.render(engine);
     }
     m_currentPos.render(engine);
@@ -363,6 +359,10 @@ void Level::handleMenuActiveEvents(Engine& engine, SDL_Event& event)
             if (cmd == "Train")
             {   
                 m_state = LevelState::RECRUITING_STATE;
+                std::pair<int, int> tilePos = m_currentPos.getPosition();
+                m_recruitingMenu.update(
+                    (tilePos.first * 16 + 15) * RENDERING_SCALE,
+                    (tilePos.second * 16 + 15) * RENDERING_SCALE);
                 m_recruitingMenu.setOptions({"Infantery", "Tank", "Artillery", "Medium", "Heavy", "Light"});
                 std::cout << "no training here" << std::endl;
             }
@@ -373,6 +373,7 @@ void Level::handleMenuActiveEvents(Engine& engine, SDL_Event& event)
         break;
     }
 }
+
 
 void Level::handleMovementEvents(Engine& engine, SDL_Event& event)
 {
