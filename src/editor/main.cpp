e1db6026c4ca1487d1ee8d3211955a5fb3619a7b
TheUltimateOptimist <digitalConfidence@web.de>
2025-02-05 01:48:17 +0100

    added documentation for main.cpp

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -13,33 +13,45 @@
 
 using namespace editor;
 
+/**
+ * The main function starts the program.
+ * It checks if any command line arguments were provided.
+ * If not it terminates. 
+ * Otherwise if a path to a hdf5 level file was provided it 
+ * starts the level editor and loads the level from the file for editing.
+ * If a width and height was provided, it create a blank level with the given dimensions.
+ */
+
 int main(int argc, char* argv[])
 {
-    // argc 1 ist der Name des Programms
     if (argc < 2)
-    { // keine Argumente übergeben
+    { // no arguments provided
         std::cerr << "Bitte uebergben Sie den Pfad zu dem Level, das sie bearbeiten wollen oder "
                      "die Breite und Hoehe des Levels, das sie neu erstellen wollen."
                   << std::endl;
         return 1;
     }
     if (argc > 3)
-    { // mehr als 2 Argumente übergeben
+    { // more than 2 arguments provided
         std::cerr << "Zuviele Kommandozeilenargumente." << std::endl;
         return 1;
     }
 
-    // Programm beginnt
+    // programm starts
     QApplication app(argc, argv);
-    SpriteProvider::initialize("../res/spritesheet.h5");
-    LevelScene* level;
 
+    // SpriteProvider is initialized once and can then be used from anywhere in the application
+    // to get the QGraphicsPixmap for a specific tile.
+    SpriteProvider::initialize("../res/spritesheet.h5");
+
+
+    LevelScene* level;
     if (argc == 2)
-    { // 1 Argument übergeben: Datei Pfad
+    { // 1 argument provided => create Level from file
         level = LevelScene::fromFile(argv[1]);
     }
     else
-    { // 2 Argumente übergeben: Breite, Höhe
+    { // 2 arguments provided => create blank level with given width and height
         level = LevelScene::empty("Mein Level", std::stoi(argv[1]), std::stoi(argv[2]));
     }
 
