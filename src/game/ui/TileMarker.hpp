db8ddd5e667439bf98daad2be34b355093623b64
Nicolas Paul Will <nicolas-paul.will@informatik.hs-fulda.de>
2025-02-05 22:23:49 +0100

    Ui interaction/building

---
+++ src/game/ui/TileMarker.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "../Scene.hpp"
+
+namespace advanced_wars
+{
+/**
+ * @class TileMarker
+ * @brief Renders a marker on top of a tile to mark current position
+ *
+ */
+class TileMarker : public Scene
+{
+    public:
+        TileMarker(int renderingScale, int tileX, int tileY, int levelWidth, int levelHeight);
+
+        void render(Engine& engine) override;
+
+        void handleEvent(Engine& engine, SDL_Event& event) override;
+
+        std::pair<int, int> getPosition();
+
+        void setPosition(int x, int y);
+
+    private:
+        int m_x;
+        int m_y;
+        int m_renderingScale;
+        int m_width;
+        int m_height;
+        int m_levelHeight;
+        int m_levelWidth;
+};
+} // namespace advanced_wars
